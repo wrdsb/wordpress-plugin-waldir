6277138e57e48c3bd380bf067df7004160e6dc02
James Schumann <james@schumann.ca>
2021-07-26 14:30:41 -0400

    add WP functions, etc., borrowed from staff-features plugin

---
+++ src/WP/WPCore.php
@@ -0,0 +1,116 @@
+<?php
+namespace WRDSB\Staff\Modules\WP;
+
+class WPCore
+{
+    public static function currentTime(string $arg = 'mysql'): string {
+        return strval(current_time($arg));
+    }
+    
+    public static function wpRemotePost(string $url, array $args = array()) {
+        return wp_remote_post($url, $args);
+    }
+
+    public static function isWPError($thing): bool {
+        return is_wp_error($thing);
+    }
+
+    public static function wpEnqueueStyle($handle, $src = '', $deps = array(), $ver = false, $media = 'all'): void {
+        wp_enqueue_style($handle, $src, $deps, $ver, $media);
+    }
+
+    public static function pluginDirURL(string $file): string {
+        return plugin_dir_url($file);
+    }
+
+    public static function wpEnqueueScript(string $handle, string $src = '', $deps = array(), $ver = false, bool $in_footer = false) {
+        wp_enqueue_script($handle, $src, $deps, $ver, $in_footer);
+    }
+
+    public static function wpLocalizeScript(string $handle, string $object_name, array $l10n): bool {
+        return wp_localize_script($handle, $object_name, $l10n);
+    }
+
+    public static function escURLRaw(string $url, $protocols = null) {
+        return esc_url_raw($url, $protocols);
+    }
+
+    public static function restURL(string $path = '', string $scheme = 'rest'): string {
+        return rest_url($path, $scheme);
+    }
+
+    public static function wpCreateNonce($action = -1): string {
+        return wp_create_nonce($action);
+    }
+
+    public static function currentUserCan(string $capability, mixed $args = null): bool {
+        return current_user_can($capability, $args);
+    }
+
+    public static function getCurrentUser() {
+        return wp_get_current_user();
+    }
+
+    public static function switchToBlog(int $new_blog_id): bool {
+        return switch_to_blog($new_blog_id);
+    }
+
+    public static function userCan($user, string $capability): bool {
+        return user_can($user, $capability);
+    }
+
+    public static function restoreCurrentBlog(): bool {
+        return restore_current_blog();
+    }
+
+    public static function registerRestRoute(string $namespace, string $route, array $args = array(), bool $override = false): bool {
+        return register_rest_route($namespace, $route, $args, $override);
+    }
+
+    public static function getOption(string $option, $default = false) {
+        return get_option($option, $default);
+    }
+
+    public static function getCurrentBlogID(): int {
+        return get_current_blog_id();
+    }
+    
+    public static function getFooter() {
+        return get_footer();
+    }
+
+    public static function homeURL() {
+        return home_url();
+    }
+
+    public static function getHeader() {
+        return get_header();
+    }
+
+    public static function getTemplatePart(string $slug, string $name = null, array $args = array()) {
+        return get_template_part($slug, $name, $args);
+    }
+
+    public static function addFilter(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1) {
+        return add_filter($tag, $function_to_add, $priority, $accepted_args);
+    }
+
+    public static function currentUserCanViewContent() {
+        return current_user_can_view_content();
+    }
+
+    public static function escURL(string $url, $protocols = null, string $_context = 'display'): string {
+        return esc_url($url, $protocols, $_context);
+    }
+
+    public static function adminURL(string $path = '', string $scheme = 'admin'): string {
+        return admin_url($path, $scheme);
+    }
+
+    public static function wpRedirect(string $location, int $status = 302, string $x_redirect_by = 'WordPress') {
+        wp_redirect($location, $status, $x_redirect_by);
+        exit;
+    }
+}
+
+
---
+++ src/WP/WPRemotePost.php
@@ -0,0 +1,100 @@
+<?php
+namespace WRDSB\Staff\Modules\WP;
+use WRDSB\Staff\Modules\WP\WPCore as WPCore;
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       https://github.com/wrdsb
+ * @since      1.0.0
+ *
+ * @package    WRDSB_Staff
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    WRDSB_Staff
+ * @author     WRDSB <website@wrdsb.ca>
+ */
+class WPRemotePost {
+    public static $successCodes = array(
+        200,
+        202
+    );
+    
+    public static $defaultHeaders = array(
+        'Accept'       => 'application/json',
+        'Content-Type' => 'application/json'
+    );
+
+    public function __construct($params) {
+        $this->timeout     = $params['timeout']     ?? 45;
+        $this->redirection = $params['redirection'] ?? 5;
+        $this->httpversion = $params['httpversion'] ?? '1.0';
+        $this->useragent   = $params['useragent']   ?? "WordPress";
+        $this->blocking    = $params['blocking']    ?? true;
+        $this->cookies     = $params['cookies']     ?? array();
+        $this->compress    = $params['compress']    ?? false;
+        $this->decompress  = $params['decompress']  ?? true;
+        $this->sslverify   = $params['sslverify']   ?? false;
+        $this->stream      = $params['stream']      ?? false;
+        $this->filename    = $params['filename']    ?? null;
+    
+        $this->url     = $params['url'];
+        $this->headers = array_merge(self::$defaultHeaders, $params['headers']);
+        $this->body    = $params['body'] ?? null;
+    }
+
+    public function run() {
+        $args = array(
+            'timeout'     => $this->timeout,
+            'redirection' => $this->redirection,
+            'httpversion' => $this->httpversion,
+            'user-agent'  => $this->useragent,
+            'blocking'    => $this->blocking,
+            'cookies'     => $this->cookies,
+            'compress'    => $this->compress,
+            'decompress'  => $this->decompress,
+            'sslverify'   => $this->sslverify,
+            'stream'      => $this->stream,
+            'filename'    => $this->filename,
+            'headers'     => $this->headers,
+            'body'        => $this->body
+        );
+
+        $retries = 0;
+        $maxRetries = 5;
+        
+        while ($retries < $maxRetries) {
+            $backoff = 5 * $retries;
+            $args['timeout'] = $args['timeout'] + $backoff;
+            $response  = WPCore::wpRemotePost($this->url, $args);
+        
+            if (is_array($response) && !empty($response) && in_array($response["response"]["code"], self::$successCodes)) {
+                break;
+            }
+            $retries++;
+        }
+        
+        if (WPCore::isWPError($response)) {
+            $this->success = false;
+            $this->status = 500;
+            $this->response = $response;
+            $this->error = $response->get_error_message();
+        } elseif (!empty($response) && in_array($response["response"]["code"], self::$successCodes)) {
+            $this->success = true;
+            $this->status = $response["response"]["code"];
+            $this->response = json_decode($response['body'], $assoc = false);
+            $this->error = null;
+        } else {
+            $this->success = false;
+            $this->status = $response["response"]["code"];
+            $this->response = json_decode($response['body'], $assoc = false);
+            $this->error = 'Unknown error.';
+        }
+    }
+}
