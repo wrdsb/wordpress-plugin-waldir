ec053acb933027f2dd1464e25ac98454f0543dee
James Schumann <james@schumann.ca>
2021-07-26 14:28:29 -0400

    add composer and dumpautoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '48434fc0735ae9644aec11cd3b2e6d188c389db8',
+    'name' => 'waldir/waldir',
+  ),
+  'versions' => 
+  array (
+    'waldir/waldir' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '48434fc0735ae9644aec11cd3b2e6d188c389db8',
+    ),
+  ),
+);
