211f22ef074923b15be291c30bc9e2601f767db4
James Schumann <james@schumann.ca>
2021-07-27 16:06:54 -0400

    add REST fields for Flenderson Person xCode fields

---
+++ src/REST/Fields/UserMeta/LocationCodes.php
@@ -0,0 +1,26 @@
+<?php
+namespace WALDIR\REST\Fields\UserMeta;
+use WALDIR\WP\WPCore;
+
+/**
+ * Define the "waldirLocationCodes" user meta field.
+ * *
+ * @link       https://github.com/wrdsb/wordpress-plugin-waldir
+ * @since      1.0.0
+ *
+ * @package    WALDIR
+ * @subpackage WALDIR/REST/Fields
+ */
+class LocationCodes {
+    public static function registerField() {
+        WPCore::registerRESTField(
+			'user',
+			'waldirLocationCodes',
+			array(
+				'get_callback'    => array(WALDIR\REST\Fields\Callbacks::class, 'usermetaGetValue'),
+				'update_callback' => array(WALDIR\REST\Fields\Callbacks::class, 'usermetaUpdateValue'),
+				'schema'          => null,
+			)
+		);
+	}
+}
---
+++ src/REST/Fields/UserMeta/SchoolCodes.php
@@ -0,0 +1,26 @@
+<?php
+namespace WALDIR\REST\Fields\UserMeta;
+use WALDIR\WP\WPCore;
+
+/**
+ * Define the "waldirSchoolCodes" user meta field.
+ * *
+ * @link       https://github.com/wrdsb/wordpress-plugin-waldir
+ * @since      1.0.0
+ *
+ * @package    WALDIR
+ * @subpackage WALDIR/REST/Fields
+ */
+class SchoolCodes {
+    public static function registerField() {
+        WPCore::registerRESTField(
+			'user',
+			'waldirSchoolCodes',
+			array(
+				'get_callback'    => array(WALDIR\REST\Fields\Callbacks::class, 'usermetaGetValue'),
+				'update_callback' => array(WALDIR\REST\Fields\Callbacks::class, 'usermetaUpdateValue'),
+				'schema'          => null,
+			)
+		);
+	}
+}
